7901e6c6ea3f90d200586326992cddd224c6728a
NJ <njorogekiragu8@gmail.com>
2025-05-25 08:38:08 +0000

    introduction to python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+number1 = 10
+number2 = 5
+
+# Sum of number1 and number2
+total = number1 + number2
+
+print(f"Addition of 10 and 5 is {total}")
+
+# Subtraction of number1 and number2
+difference = number1 - number2
+
+print(f"Subtraction of 10 and 5 is {difference}")
+
+# Multiplication of number1 and number2
+product = number1 * number2
+
+print(f"Multiplication of 10 and 5 is {product}")
