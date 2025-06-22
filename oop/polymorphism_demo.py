d9ef3aaeeac43314fb2c6ba88f2ed62ac3f895cf
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-06-22 15:34:02 +0300

    oop

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,29 @@
+from math import pi 
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        try:
+            print(self.area())
+        except:
+            raise NotImplementedError
+        
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius * self.radius
+    
